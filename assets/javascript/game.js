b49b5d4d74b744e692519b28bfebbc332b2ef864
VJNap <vjnapolitano1364@gmail.com>
2019-06-29 23:05:10 -0500

    VN - Began formatting the html for the game page, organized and restructured the layout of  the js file and psuedocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,3 @@
-// wins, losses, guesses left, array of words to choose from, chosen word, already guessed letters array, selected word array with spaces, lettersFound, lettersTotal, resetVariablesandUIFunction
-
-// variables
-
-var wordBank = ["chiffonade", "saute", "poach", "bacon", "souffle"];
-
-//
-
-var chosenWord = "";
-var lettersInChosenWord = [];
-var numBlanks = 0;
-
-
-
-
-
-//
-
-
-var gameBoard = [];
-for (var i = 0; i < word.length; i++) {
-    gameBoard[i] = "_";
-}
-
-var lettersLeft = word.length;
-
-// working key function //
-
-document.onkeyup = function(event) {
-
-    if (event.keyCode >= 65 && event.keyCode =< 90) {
-
-  
-
-    var playerInput = event.key;
-
-    alert("You chose " + playerInput);
-  }
-}
-
-// playerInput terms //
-
-    // if found in word: populate space, add to guessed array, subtract from guesses left
-    
-        // if word is completed: add to wins, reset word, play and stop sound
-
-    // if not found in word: add to guessed array, subtract from guesses left
-
-        // if out of guesses: add to losses, reset word
-
-    // if in guessed array: try again
-
 // UI Display //
 
     // "Press any key to get started!"
@@ -66,10 +14,21 @@ document.onkeyup = function(event) {
 
 // Initialize variables
 
+var wordBank = ["chiffonade", "saute", "poach", "bacon", "souffle"];
+var chosenWord = "";
+var lettersInChosenWord = [];
+var numBlanks = 0;
+var lettersLeft = chosenWord.length;
+var gameBoard = [];
+for (var i = 0; i < chosenWord.length; i++) {
+    gameBoard[i] = "_";
+}
 
+// =========================================
 
 // Initialize functions
 
+    // start function
 function startGame() {
     numGuesses = 9;
 
@@ -86,9 +45,38 @@ function startGame() {
 
     wrongGuesses = [];
 
-    for (var i = 0: i < numBlanks; i++) {
+    for (var i = 0; i < numBlanks; i++) {
 
     }
 }
 
+    // working key function
+
+document.onkeyup = function(event) {
+
+    // if (event.keyCode >= 65 && event.keyCode < 90); {
+        
+        var playerInput = event.key;
+
+        alert("You chose " + playerInput);
+
+        // }
+
+    return playerInput;
+}
+
+// ===========================================
+
 // Process
+
+    // playerInput terms
+
+        // if found in word: populate space, add to guessed array, subtract from guesses left
+        
+            // if word is completed: add to wins, reset word, play and stop sound
+
+        // if not found in word: add to guessed array, subtract from guesses left
+
+            // if out of guesses: add to losses, reset word
+
+        // if in guessed array: try again
